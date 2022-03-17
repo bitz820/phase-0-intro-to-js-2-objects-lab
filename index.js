d8c8e934f5d21e11f5509e73bb4a943b7b5bb45c
Mark <mark.n.bacon.jr@gmail.com>
2022-03-16 22:31:53 -0400

    commit tree 400d933c274bbb43ece5c9df233c5671d4e9a10a

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+let employee = {
+    name: "Brian",
+    steetAddress: '715 W 172St',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, val) {
+    let newObj = {
+        ...obj,
+        [key]: val
+    }
+    return newObj
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, val){
+    obj[key] = val;
+    return obj
+};
+
+function deleteFromEmployeeByKey(obj, key){
+    let newObj = {
+        ...obj
+    }
+    delete newObj[key]
+    return newObj
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj
+}
+
